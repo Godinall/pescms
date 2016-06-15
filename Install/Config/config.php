86d1ee184d37c22cf10fadf211963ff9102f5698
l603430 <l20110822@gmail.com>
2016-06-15 15:52:27 +0800

    first commit

---
+++ Install/Config/config.php
@@ -0,0 +1,16 @@
+<?php
+ return array(
+'DB_TYPE' => 'mysql',
+'DB_HOST' => '192.168.1.63',
+'DB_NAME' => 'cms_ticket',
+'DB_USER' => 'root',
+'DB_PWD' => 'bmob2013',
+'DB_PORT' => '3306',
+'DB_PREFIX' => 'pes_',
+'PRIVATE_KEY' => '0310f5a049',
+'USER_KEY' => '95e5c01135',
+'APP_GROUP_LIST' => 'Install',
+'DEFAULT_GROUP' => 'Install',
+'ERROR_PROMPT' => '/Core/Theme/error.php',
+'LOG_PATH' => '/log',
+);
---
+++ Install/Config/config_tmp.php
@@ -0,0 +1,20 @@
+<?php
+ return array(
+'DB_TYPE' => 'mysql',
+'DB_HOST' => '192.168.1.63',
+'DB_NAME' => 'cms_ticket',
+'DB_USER' => 'root',
+'DB_PWD' => 'bmob2013',
+'DB_PORT' => '3306',
+'DB_PREFIX' => 'pes_',
+'PRIVATE_KEY' => '0310f5a049',
+'USER_KEY' => '95e5c01135',
+'ERROR_PROMPT' => '/Core/Theme/error.php',
+'APP_GROUP_LIST' => 'Form,Ticket',
+'DEFAULT_GROUP' => 'Form',
+'FILE_CACHE_PATH' => '/Temp',
+'FILE_CACHE_TIME' => '1800',
+'LOG_PATH' => '/log',
+'LOG_DELETE' => '7',
+'UPLOAD_PATH' => '/upload',
+);
